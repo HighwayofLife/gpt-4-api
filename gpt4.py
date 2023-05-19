92a18244e3337c9b53b6be691c6399bdaaa8555e
David lewis <highwayoflife@gmail.com>
2023-05-19 08:51:07 -0700

    Multiple inputs, handle errors, correct stream
    * Will now concat multiple file inputs as a single prompt, in order that they were provided.
    * Handles a number of errors coming from OpenAI if they are encountered
    * Correctly handles streamed responses and flushes them immediately when stream enabled

--- gpt4.py
+++ gpt4.py
@@ -4,33 +4,33 @@ import argparse
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-MODEL = 'gpt-4'
+MODEL = "gpt-4"
 MAX_TOKENS = 4500
 TEMPERATURE = 1
 STREAM = False
 
 
 def handle_args(args):
-    if os.environ.get('OPENAI_MODEL'):
-        set_model_config_model(os.environ.get('OPENAI_MODEL'))
+    if os.environ.get("OPENAI_MODEL"):
+        set_model_config_model(os.environ.get("OPENAI_MODEL"))
 
     if args.model:
         set_model_config_model(args.model)
 
-    if os.environ.get('OPENAI_MAX_TOKENS'):
-        set_model_config_max_tokens(os.environ.get('OPENAI_MAX_TOKENS'))
+    if os.environ.get("OPENAI_MAX_TOKENS"):
+        set_model_config_max_tokens(os.environ.get("OPENAI_MAX_TOKENS"))
 
     if args.max_tokens:
         set_model_config_max_tokens(args.max_tokens)
 
-    if os.environ.get('OPENAI_TEMPERATURE'):
-        set_model_config_temperature(os.environ.get('OPENAI_TEMPERATURE'))
+    if os.environ.get("OPENAI_TEMPERATURE"):
+        set_model_config_temperature(os.environ.get("OPENAI_TEMPERATURE"))
 
     if args.temperature:
         set_model_config_temperature(args.temperature)
 
-    if os.environ.get('OPENAI_STREAM'):
-        set_model_config_stream(os.environ.get('OPENAI_STREAM'))
+    if os.environ.get("OPENAI_STREAM"):
+        set_model_config_stream(os.environ.get("OPENAI_STREAM"))
 
     if args.stream:
         set_model_config_stream(args.stream)
@@ -71,12 +71,19 @@ def call_gpt_4(input_text):
             max_tokens=MAX_TOKENS,
             temperature=TEMPERATURE,
             messages=messages,
-            stream=STREAM
+            stream=STREAM,
         )
         return response
-    except openai.Error as e:
-        print(f"An error occurred: {e}")
-
+    except openai.error.InvalidRequestError as e:
+        print(f"Invalid request: {e}")
+    except openai.error.AuthenticationError as e:
+        print(f"Authentication error: {e}")
+    except openai.error.APIConnectionError as e:
+        print(f"Network error: {e}")
+    except openai.error.OpenAIError as e:
+        print(f"Unknown error: {e}")
+    except Exception as e:
+        print(f"Unknown error: {e}")
 
 def print_response(input_text):
     response = call_gpt_4(input_text)
@@ -86,39 +93,42 @@ def print_response(input_text):
         # If streaming, the response is an iterable
         print(" ", end="")
         for message in response:
-            if 'content' in message['choices'][0]['delta']:
-                print(message['choices'][0]['delta']
-                      ['content'], end='', flush=True)
-        print("\n\n-----------------")  # print a newline at the end
+            if "content" in message["choices"][0]["delta"]:
+                print(message["choices"][0]["delta"]["content"], end="", flush=True)
+        print("\n\n-----------------")
     else:
         print(response)
 
 
 def read_from_file(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         return file.read()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='Send a file prompt to GPT-4')
-    parser.add_argument('input_files', nargs='+',
-                        help='Input files to use for a prompt')
-    parser.add_argument('--model', type=str, default=None,
-                        help='OpenAI Chat Model to use')
-    parser.add_argument('--max_tokens', type=int, default=None,
-                        help='Max tokens to use')
-    parser.add_argument('--temperature', type=float, default=None,
-                        help='Temperature to use')
-    parser.add_argument('--stream', type=bool, default=None,
-                        help='Stream the output')
+    parser = argparse.ArgumentParser(description="Send a file prompt to GPT-4")
+    parser.add_argument(
+        "input_files", nargs="+", help="Input files to use for a prompt"
+    )
+    parser.add_argument(
+        "--model", type=str, default=None, help="OpenAI Chat Model to use"
+    )
+    parser.add_argument(
+        "--max_tokens", type=int, default=None, help="Max tokens to use"
+    )
+    parser.add_argument(
+        "--temperature", type=float, default=None, help="Temperature to use"
+    )
+    parser.add_argument("--stream", type=bool, default=None, help="Stream the output")
 
     args = parser.parse_args()
 
     handle_args(args)
 
-    for file_path in args.input_files:
-        print("Prompting with file:", file_path)
+    print("Prompting with files:", args.input_files)
 
-        input_text = read_from_file(file_path)
-        print_response(input_text)
+    input_text = ""
+    for file_path in args.input_files:
+        input_text += read_from_file(file_path)
+
+    print_response(input_text)
