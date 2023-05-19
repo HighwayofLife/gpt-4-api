906942dd073881f51cb4820c0f5fc8123e6484b1
David lewis <highwayoflife@gmail.com>
2023-05-18 17:11:53 -0700

    Use an optional token count as a means to chunk

--- chunk_file.py
+++ chunk_file.py
@@ -6,6 +6,7 @@ import tiktoken
 WORD_LIMIT = 3000
 TOKEN_LIMIT = 0
 GPT_MODEL = "gpt-4"
+ENCODING = None
 
 
 def config_set_word_limit(word_limit):
@@ -23,14 +24,20 @@ def config_set_gpt_model(gpt_model):
     GPT_MODEL = gpt_model
 
 
+def config_set_encoding_for_model():
+    global ENCODING
+
+    ENCODING = tiktoken.encoding_for_model(GPT_MODEL)
+
+
 def count_tokens(input_text):
-    encoding = tiktoken.encoding_for_model(GPT_MODEL)
-    return len(encoding.encode(input_text))
+    if not ENCODING:
+        config_set_encoding_for_model()
+
+    return len(ENCODING.encode(input_text))
 
 
 def split_file(file_path):
-    words_per_file = WORD_LIMIT
-
     # check if the file exists
     if not os.path.isfile(file_path):
         print(f"File: {file_path} does not exist.")
@@ -39,34 +46,42 @@ def split_file(file_path):
     with open(file_path, 'r') as f:
         file_num = 1
         word_count = 0
+        token_count = 0
         output_file = None
+        current_line = ""
 
         for line in f:
             words = line.strip().split()
-            if word_count + len(words) > words_per_file or not output_file:
-                if output_file:
-                    # Show the file being written and the number of words written to that file
-                    print(
-                        f"Writing to file: {new_file_name}, with {word_count} words.")
-                    output_file.close()
+            new_token_count = token_count + count_tokens(line)
+            new_word_count = word_count + len(words)
 
+            if ((TOKEN_LIMIT > 0 and new_token_count > TOKEN_LIMIT) or new_word_count > WORD_LIMIT) and output_file:
+                # Show the file being written and the number of words written to that file
+                print(
+                    f"Writing to file: {new_file_name}, with {word_count} words and {token_count} tokens.")
+                output_file.close()
+                output_file = None
+
+            if not output_file:
                 # open new output file
                 # Strip the extension from the original file name and append a number
                 new_file_name = os.path.splitext(
                     file_path)[0] + f".{file_num}.txt"
                 output_file = open(new_file_name, 'w')
-
                 file_num += 1
                 word_count = 0
+                token_count = 0
 
             # write line to current output file
-            output_file.write(line)
+            output_file.write(current_line)
             word_count += len(words)
+            token_count += count_tokens(current_line)
+            current_line = line
 
         if output_file:
             # Show the file being written and the number of words written to that file
             print(
-                f"Writing to file: {new_file_name}, with {word_count} words.")
+                f"Writing to file: {new_file_name}, with {word_count} words and {token_count} tokens.")
             output_file.close()
 
 
